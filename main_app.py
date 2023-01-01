765fb1173ab32698464d74d4f8b145f8950f47a5
Shoshi Finkel <sfinkel@integralytic.com>
2023-01-01 17:16:42 +0200

    change

--- main_app.py
+++ main_app.py
@@ -1,12 +1,21 @@
 # Final Project
-# planning...
+
+# My plan is to first explore the data. For this purpose, I will print head, info and describe.
+#In addition, I will check if there are nulls and I will count the values of some of the columns
+#After that I will clean the data. I will delete irrelevant columns and replace the missing values
+#After that I will analyze the information with graphs
+#I will create graphs that will show me the information in different parallels so that I can analyze the information.
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotting_functions
-import analysis_functions
+from plotting_functions import create_heatmap
+from analysis_functions import explore_data
 
 df = pd.read_csv("hotel_bookings.csv")
+
+explore_data(df)
+
+create_heatmap(df, ['lead_time','is_canceled'])
 
 
--- plotting_functions.py
+++ plotting_functions.py
@@ -1,3 +1,7 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
 def create_heatmap(df, cols):
-    sns.heatmap(data=df[cols].corr(),annot=True, cmap='Purples')
-    plt.suptitle("Heatmap of", cols)
+    sns.heatmap(data=df[cols].corr(),annot=True, cmap='Purples').set(title = 'Heatmap of {} columns'.format(cols))
+    plt.savefig('heatmap.png')
