949a682aa0fbdaa7d9d7d5a916765755c9581167
Shoshi Finkel <sfinkel@integralytic.com>
2023-01-01 12:19:53 +0200

    create main app and import libraries

---
+++ main_app.py
@@ -0,0 +1,12 @@
+# Final Project
+# planning...
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotting_functions
+import analysis_functions
+
+df = pd.read_csv("hotel_bookings.csv")
+
+
