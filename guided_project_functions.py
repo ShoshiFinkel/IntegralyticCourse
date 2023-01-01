98cc9a8d68d499d7c96e40f5cc9bf803112aca71
Shoshi Finkel <sfinkel@integralytic.com>
2023-01-01 10:10:46 +0200

    a

--- guided_project_functions.py
+++ guided_project_functions.py
@@ -6,7 +6,7 @@ import seaborn as sns
 hotel_bookings = pd.read_csv('hotel_bookings.csv')
 
 def count_nulls(col):
-    hotel_bookings[col].isnull().sum()
+    print(hotel_bookings[col].isnull().sum())
 
 def create_catplot(col, col1):
     sns.catplot(x=col, kind='count', col=col1, palette='rainbow', data=hotel_bookings).set(title = 'Bar chart of {} per {}'.format(col,col1))
