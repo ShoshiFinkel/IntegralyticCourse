06b1ee7f108f6b3e5181d5aa1558dec21c5636c0
Shoshi Finkel <sfinkel@integralytic.com>
2023-01-01 09:56:15 +0200

    create_two_functions

--- guided_project_functions.py
+++ guided_project_functions.py
@@ -8,3 +8,6 @@ hotel_bookings = pd.read_csv('hotel_bookings.csv')
 def count_nulls(col):
     hotel_bookings[col].isnull().sum().sort_values(ascending=False)
 
+def create_catplot(col, col1):
+    sns.catplot(x=col, kind='count', col=col1,palette='rainbow', data=hotel_bookings).set(title = 'Bar chart of {} column'.format(col))
+    plt.show()
