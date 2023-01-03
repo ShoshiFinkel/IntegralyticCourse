10b549f2081440a91977d61fc9ea072e542c51d0
Shoshi Finkel <sfinkel@integralytic.com>
2023-01-03 12:59:38 +0200

    markdown

--- analysis_functions.py
+++ analysis_functions.py
@@ -1,20 +1,24 @@
 import pandas as pd
 
+# Display information about the data: head, info, describe and count the nulls. The argument is the dataframe.
 def display_info(df):
     print(df.head())
     print(df.info())
     print(df.describe())
     print(df.isnull().sum().sort_values(ascending=False))
 
+# Check if there are nulls. The arguments are the dataframe and the column.
 def count_nulls(df,col):
     sum_nulls = df[col].isnull().sum()
     print(sum_nulls)
     return sum_nulls
 
+# Drop columns. The arguments are the dataframe and column.
 def drop_col(df,col):
     df.drop(col,axis=1, inplace = True)
     return df
 
+# Clean the null values. if there are more than 500 null values in a column- drop the column, else- fill the nuns with the argument. The arguments are the dataframe, the column, and the value to fill the nulls with.
 def clean_nulls(df,col,fillnans):
     sum_nulls = count_nulls(df,col)
     if sum_nulls >500:
@@ -24,15 +28,18 @@ def clean_nulls(df,col,fillnans):
         df[col] =df[col].fillna(fillnans)
         return df
 
+# Change the month's names to numbers. The arguments are the dataframe and the column(should be the month column)
 def month_in_num(df, month_col):
     df[month_col] = df[month_col].map({'January' : 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June':6, 'July':7, 'August':8, 'September':9, 'October':10, 'November': 11, 'December':12})
     return df
 
+# Create a column of arrival date, including day, month and year. The arguments are the dataframe, new column name, year column, month column, date column.
 def datetime_col(df, new_col, year_col, month_col, day_col):
     df['dateInt']=df[year_col].astype(str) + df[month_col].astype(str).str.zfill(2)+ df[day_col].astype(str).str.zfill(2)
     df[new_col] = pd.to_datetime(df['dateInt'], format='%Y%m%d')
     return df
 
+# Count the values in a column. The arguments are the dataframe, column, and number of rows to print in the .head() function.
 def col_value_counts(df,col,head_rows):
     result = df[col].value_counts().head(head_rows)
     print(result)
--- plotting_functions.py
+++ plotting_functions.py
@@ -2,14 +2,13 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Create heatmap to find the correlation between few columns. The arguments are the dataframe, columns as a list, the plot name.
 def create_heatmap(df, cols, plot_name):
     sns.heatmap(data=df[cols].corr(),annot=True, cmap='Purples').set(title = plot_name)
     return plt.savefig(plot_name+'.png')
 
+# Create catplot. The arguments are the dataframe, column for the x parameter, column for the col parameter, and the plot name.
 def create_catplot(df, col, col1, plot_name):
     sns.catplot(x=col, kind='count', col=col1, palette='rainbow', data=df).set(title =plot_name)
     plt.savefig(plot_name+'.png')
 
-#def create_catplot(df, xcol, kind1, plot_name, y=None, col=None,  hue=None):
- #   sns.catplot(x=xcol, y=None, kind=kind1, col=None ,hue=None, palette='rainbow', data=df).set(title = plot_name)
-  #  plt.savefig(plot_name+'.png')
