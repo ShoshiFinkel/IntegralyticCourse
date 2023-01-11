e20e2f610fb1602b6a98eb4aaa8dfce814fd9706
Shoshi Finkel <sfinkel@integralytic.com>
2023-01-11 14:00:12 +0200

    11/1/23

--- OOP/CreditCardAssigment.py
+++ OOP/CreditCardAssigment.py
@@ -1,21 +1,35 @@
-# ● Bonus: Include validation. For example, cannot be a future date.
-# ● Bonus 2: define an additional class. Use it inside the CreditCardStatement class.
-# ● Have fun with it! Include concepts that you’ve learned in other classes.
 import pandas as pd
-import datetime
 class CreditCardStatment:
     df = pd.DataFrame()
-    def __init__(self, owner_name, cc_type):
-        self.owner_name = owner_name
-        self.cc_type = cc_type
+    def __init__(self):
+        self.__owner_name = 'Shoshi'
+        self.__cc_company = 'visa'
+        self.__file_path = 'OOP\credit_card_bill.txt'
         
-    def read_file(self, file_path):
-        self.df=pd.read_csv(file_path, header=None)
+    def read_file(self):
+        self.df=pd.read_csv(self.__file_path, header=None)
 
     def display_statment(self):
-        for index, row in self.df.iterrows():
-            display_statment = "In "+ str(row[0])+ " " + str(row[1])+" NIS was spent on "+ str(row[2])
-            print(display_statment)
+        if self.df.empty:
+            print("The dataframe is empty, please read the data.")
+        else:
+            for index, row in self.df.iterrows():
+                display_statment = "In "+ str(row[0])+ " " + str(row[1])+" NIS was spent on "+ str(row[2])
+                print(display_statment)
+        
+            
 
     def __str__(self):
-        return "This is {}'s {} credit card.".format(self.owner_name, self.cc_type)
+        return "This is {}'s {} credit card.".format(self.__owner_name, self.__cc_company)
+
+    def set_name(self, new_name):
+        self.__owner_name = new_name
+        return self.__owner_name
+
+    def set_compant_name(self, new_company_name):
+        self.__cc_company = new_company_name
+        return self.__cc_company
+
+    def set_file_path(self, new_file_path):
+        self.__file_path = new_file_path
+        return self.__file_path
--- OOP/CreditCardImport.py
+++ OOP/CreditCardImport.py
@@ -2,9 +2,15 @@ import pandas as pd
 from CreditCardAssigment import CreditCardStatment
 
 def main():
-    shoshi_cc = CreditCardStatment('Shoshi Finkel', 'visa')
+    shoshi_cc = CreditCardStatment()
+    shoshi_cc.__owner_name = 'Sara'
+    shoshi_cc.set_name('Sara')
+    shoshi_cc.__cc_company = "diners"
+    shoshi_cc.set_compant_name("diners")
+    shoshi_cc.__df = 'OOP\credit_card_bill.txt'
+    shoshi_cc.set_file_path("OOP\credit_card_bill.txt")
     print(shoshi_cc)
-    shoshi_cc.read_file('OOP\credit_card_bill.txt')
+    shoshi_cc.read_file()
     shoshi_cc.display_statment()
 
 main()
