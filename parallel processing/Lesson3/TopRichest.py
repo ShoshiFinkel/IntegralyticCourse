bb151459c2dea38f264212b4656aaf0b0d0d4e8f
Shoshi Finkel <sfinkel@integralytic.com>
2023-01-24 23:46:43 +0200

    24/1/23

--- parallel processing/Lesson3/TopRichest.py
+++ parallel processing/Lesson3/TopRichest.py
@@ -1,3 +1,7 @@
 import pandas as pd
-df = pd.read_csv('TopRichestInWorld.csv')
-df.head()
+df = pd.read_csv('parallel processing\Lesson3\TopRichestInWorld.csv')
+
+working_in_tech = df['Industry'].str.count('Technology').sum()
+industries = {}
+industries['Technology'] = working_in_tech
+
