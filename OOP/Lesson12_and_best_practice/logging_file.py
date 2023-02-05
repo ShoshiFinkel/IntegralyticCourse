2bb31115d3185435c120b7d9b436ab5c590159f6
Shoshi Finkel <sfinkel@integralytic.com>
2023-02-05 17:44:29 +0200

    /

--- OOP/Lesson12_and_best_practice/logging_file.py
+++ OOP/Lesson12_and_best_practice/logging_file.py
@@ -1,6 +1,14 @@
 import os
 import sys
+
+# CURR_DIR = os.path.dirname(__file__)
+# sys.path.append(CURR_DIR)
+# LOG_FOLDER =  CURR_DIR + '/logging'
+# LOG_FILE = LOG_FOLDER +  '/camp_logs.txt'
+
 CURR_DIR = os.path.dirname(__file__)
 sys.path.append(CURR_DIR)
-LOG_FOLDER =  CURR_DIR + '/logging'
-LOG_FILE = LOG_FOLDER +  '/camp_logs.txt'
+#INFO_FOLDER = CURR_DIR + '/wedding'
+LOG_FOLDER =  CURR_DIR + '/logs'
+#HALLS_INFO_FILE = 'halls.csv'
+LOG_FILE = 'camp_logs.txt'
--- OOP/Lesson12_and_best_practice/main.py
+++ OOP/Lesson12_and_best_practice/main.py
@@ -3,6 +3,11 @@ import logging
 import logging_file as lf
 import traceback
 
+logging.basicConfig(filename =  lf.LOG_FILE,
+                    filemode = 'a+',
+                    format = '%(asctime)s ,%(msecs)d %(name)s  %(levelname)s  %(message)s',
+                    datefmt = '%H:%M:%S',
+                    level = logging.INFO)
 
 
 def main():
@@ -23,37 +28,61 @@ def main():
                     logging.info("Added counselor")
                 except ValueError as exc:
                     logging.error(traceback.format_exc())
-                    hire_date = input('Hire date was bad format, please re-enter(yyyy-MM-dd: ')
-                    our_camp.add_counselor(fname, lname, hire_date, salary)
-                    logging.info("Added counselor after change")
+                hire_date = input('Hire date was bad format, please re-enter(yyyy-MM-dd: ')
+                our_camp.add_counselor(fname, lname, hire_date, salary)
+                logging.info("Added counselor after change")
 
             elif menu_choice == 2: #add bunk
                 bunk_name = input('Enter bunk name:')
                 counselor_fname = input('Enter counselor\'s first name:' )
                 counselor_lname = input('Enter last name:')
                 our_camp.add_bunk(bunk_name, counselor_fname, counselor_lname)
+                logging.info("bunk added successfully")
+
             elif menu_choice == 3: #add camper
                 fname = input('Enter camper first name:')
                 lname = input('Enrer last name:')
                 dob = input('Enter day of birth (dd-mm-yyyy):')
+                try:
+                    our_camp.add_camper(fname, lname, dob)
+                    logging.info("camper added successfully")
+                except ValueError as exc:
+                    logging.error(str(exc)+traceback.format_exc())
+                dob=input('Date of birth was bad format,please re-enter (dd/mm/yyyy):')
                 our_camp.add_camper(fname, lname, dob)
+                logging.info("camper added successfully after change")
+
+
             elif menu_choice == 4: #add an allergy
                 fname = input('Enter camper first name:')
                 lname = input('Enter camper last name:')
                 allergy = input('Add allergy:')
                 our_camp.add_allergy(fname, lname, allergy)
+                logging.info("allergy added successfully")
+
             elif menu_choice == 5: #assign camper to bunk
                 fname = input('Enter camper first name:')
                 lname = input('Enter camper last name:')
                 dob = dob = input('Enter day of birth (dd-mm-yyyy):') 
                 bunk_name = input('Enter bunk name:')
                 our_camp.place_camper(fname, lname, bunk_name)
+                logging.info("camper assigned successfully")
+                
             elif menu_choice == 6: #
                 print(our_camp)
+                logging.info("printed __str__ of our_camp")
+
             elif menu_choice == 0:
                 print('You exited the application')
+                logging.info('You exited the application')
+                
         except Exception as ex:
             print('Error!'+str(ex))
+            traceback.print_exc()
+            logging.error(str(ex))
+            logging.error(traceback.format_exc())
+        else:
+            logging.info('task completed successfully')
                     
 def get_menu_choice():
     menu = "\n1. Add A Counselor"
